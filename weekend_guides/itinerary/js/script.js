45b4d489998257a15095e7c402ced0405e483cc2
Kartikye Mittal <kartikye.mittal@gmail.com>
2018-03-20 00:27:58 -0700

    sing

--- weekend_guides/itinerary/js/script.js
+++ weekend_guides/itinerary/js/script.js
@@ -51,7 +51,6 @@ function registerHandler(element, map, marker){
 	})
 	$(element).on('click', function(){
 		p = marker[0].getPosition()
-		console.log(p)
 		map.setCenter(p)
 		map.setZoom(map_data.zoom+1)
 	})
@@ -82,306 +81,75 @@ function loadTransportation(item){
 	$('#content').append(content_string)
 }
 
+function setStyles(styles){
+	s = "<style>#items #content .item .label_section .label{background-color: {{color}};} #items #content .item .label_section .label_icon{color: {{color}};} #items #content .item .label_section .line{border-color: {{color}};} </style>".replace(/{{color}}/g, styles.highlight)
+	$('head').append(s)
+}
+
 function loadInformation(data){
+	setStyles(data.styles)
 	var itinerary_data = data.itineraries[itinerary]
 
 	$('title').text(data.location + ' - ' + itinerary_data.name)
 	$('#title').text(data.location)
 	$('#subtitle').text(itinerary_data.name)
-
+	
 	map_data = data.map
 	console.log(map_data)
-	var map = createMap("map", map_data.location, map_data.zoom, map_data.styles)
+
+	zoom = map_data.zoom
+
+	if ("zoom" in itinerary_data){
+		zoom = itinerary_data[zoom]
+	}
+
+	var map = createMap("map", map_data.location, zoom, map_data.styles)
 	var markers = []
+
+	var lat_sum = 0;
+	var lng_sum = 0;
+	var i = 0;
+
 
 	itinerary_data.itinerary.forEach(function(item){
 		console.log(item)
 		if (item.type == "sight") {
 			m = addMarker(item, map)
 			markers.push(m)
+			lat_sum += item.location.lat
+			lng_sum += item.location.lng
 			loadSight(item, map, m)
 		} else if (item.type == "food") {
 			m = addMarker(item, map)
 			markers.push(m)
+			lat_sum += item.location.lat
+			lng_sum += item.location.lng
 			loadFood(item, map, m)
 		} else if (item.type == "drive", "bus", "train", "walk") {
 			loadTransportation(item)
 		}
-	})	
+
+		if (i == itinerary_data.itinerary.length-1){
+			map.setCenter(new google.maps.LatLng(lat_sum/markers.length, lng_sum/markers.length))
+		}
+        
+        i+=1
+	})
+
 }
 
 var hash = window.location.hash.substring(1)
 var location_name = hash.substring(0, hash.indexOf('/'))
 var itinerary = hash.substring(hash.indexOf('/')+1)
-
+console.log()
 $.ajax({
 	dataType: "json",
 	url: "../" + location_name +"/itinerary.json",
 	success: function(data){
+		console.log(data)
 		loadInformation(data)
 	},
 	error: function(e){
-	console.log("HUKLLO")
-  		loadInformation({
-	"location": "San Francisco",
-	"itineraries": {
-		"mission": {
-			"name": "The Mission District",
-			"itinerary": [
-				{
-					"name": "Mission Dolores Park",
-					"location": {
-						"lat": 37.7596168,
-						"lng": -122.42690379999999
-					},
-					"label": "A",
-					"type": "sight",
-					"cost": "Free",
-					"time": "1.5 hours",
-					"description": "Bustling park where people gather for picincs and relaxation.",
-					"link": "http://sfrecpark.org/destination/mission-dolores-park/",
-					"link_text": "sfrecpark.org",
-					"image": ""
-				},
-				{
-					"type": "walk",
-					"duration": "19 minute walk"
-				},
-				{
-					"name": "Taquería El Farolito",
-					"location": {
-						"lat": 37.752654782767,
-						"lng": -122.41819202899933
-					},
-					"label": "B",
-					"type": "food",
-					"cost": "$$",
-					"description": "Busy Mexican restaurant serving comfort food.",
-					"link": "http://places.singleplatform.com/el-farolito-3/menu?ref=google",
-					"link_text": "Menu",
-					"image": ""
-				},
-				{
-					"name": "Mission District Murals",
-					"location": {
-						"lat": 37.752420993840076,
-						"lng": -122.41570642570304
-					},
-					"label": "C",
-					"type": "sight",
-					"cost": "Free",
-					"time": "1.5 hours",
-					"description": "A multitude of everchanging murals located in the Mission.",
-					"link": "",
-					"link_text": "",
-					"image": ""
-				},
-				{
-					"name": "Foreign Cinema Restaurant",
-					"location": {
-						"lat": 37.752420993840076,
-						"lng": -122.41570642570304
-					},
-					"label": "D",
-					"type": "food",
-					"cost": "$$$",
-					"description": "Californian-Mediterranean food in an outdoor space screening films.",
-					"link": "Website",
-					"link_text": "foreigncinema.com",
-					"image": ""
-				},
-				{
-					"name": "Bi Rite Creamery",
-					"location": {
-						"lat": 37.76158999999999,
-						"lng": -122.42571700000002
-					},
-					"label": "E",
-					"type": "food",
-					"cost": "$",
-					"description": "Homemade organic ice cream with often long lines.",
-					"link": "Website",
-					"link_text": "biritecreamery.com",
-					"image": ""
-				},
-				{
-					"name": "Twin Peaks",
-					"location": {
-						"lat": 37.7544066,
-						"lng": -122.44768449999998
-					},
-					"label": "G",
-					"type": "sight",
-					"cost": "Free",
-					"time": "30 minutes",
-					"description": "An amazing, 180 degree view of the Bay Area.",
-					"link": "",
-					"link_text": "",
-					"image": ""
-				}
-			]
-		}
-	},
-	"map": {
-		"location": {
-			"lat": 37.75777584134365,
-			"lng": -122.4341467629751
-		},
-		"zoom": 13,
-		"styles": [
-			{
-				"elementType": "labels",
-				"stylers": [
-			  		{
-						"color": "#f3f3f3"
-			  		}
-				]
-		  	},
-		  	{
-				"elementType": "labels.text.stroke",
-				"stylers": [
-					{
-						"color": "#949494"
-				  	},
-				  	{
-						"weight": 2.5
-				  	}
-				]
-		  	},
-		  	{
-				"featureType": "administrative",
-				"elementType": "geometry",
-				"stylers": [
-				  	{
-						"visibility": "off"
-				  	}
-				]
-			},
-			{
-				"featureType": "administrative.land_parcel",
-				"elementType": "labels",
-				"stylers": [
-			  		{
-						"visibility": "off"
-			  		}
-				]
-		  	},
-		  	{
-				"featureType": "landscape",
-				"elementType": "geometry",
-				"stylers": [
-			  		{
-						"color": "#ff766d"
-			  		}
-				]
-		  	},
-		  	{
-				"featureType": "poi",
-				"stylers": [
-			  		{
-						"visibility": "off"
-			  		}
-				]
-		  	},
-		  	{
-				"featureType": "poi",
-				"elementType": "labels.text",
-				"stylers": [
-			  		{
-						"visibility": "off"
-			  		}
-				]
-		  	},
-		  	{
-				"featureType": "road",
-				"elementType": "geometry.fill",
-				"stylers": [
-			  		{
-						"color": "#ffffff"
-			  		}
-				]
-		  	},
-		  	{
-				"featureType": "road",
-				"elementType": "geometry.stroke",
-				"stylers": [
-			  		{
-						"color": "#ffffff"
-			  		},
-			  		{
-						"weight": 0.5
-			  		}
-				]
-		  	},
-		  	{
-				"featureType": "road",
-				"elementType": "labels.icon",
-				"stylers": [
-				  	{
-						"visibility": "off"
-				  	}
-				]
-		  	},
-		  	{
-				"featureType": "road.arterial",
-				"elementType": "labels",
-				"stylers": [
-			  		{
-						"visibility": "off"
-			  		}
-				]
-		  	},
-		  	{
-				"featureType": "road.highway",
-				"elementType": "labels",
-				"stylers": [
-				  	{
-						"visibility": "off"
-				  	}
-				]
-		  	},
-		  	{
-				"featureType": "road.local",
-				"stylers": [
-				  	{
-						"visibility": "off"
-				  	}
-				]
-		  	},
-		  	{
-				"featureType": "road.local",
-				"elementType": "labels",
-				"stylers": [
-				  	{
-						"visibility": "off"
-				  	}
-				]
-		  	},
-		  	{
-				"featureType": "transit",
-				"stylers": [
-				  	{
-						"visibility": "off"
-				  	}
-				]
-		  	},
-		  	{
-				"featureType": "water",
-				"stylers": [
-				  	{
-						"color": "#54dede"
-				  	}
-				]
-		  }
-		]
+		console.log(e)
 	}
-})
-  	}
 });
-
-
-
-
-
-
-//[{"elementType":"labels","stylers":[{"color":"#f3f3f3"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#949494"},{"weight":2.5}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"administrative.land_parcel","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#ff766d"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"weight":0.5}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.local","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"color":"#54dede"}]}]
--- weekend_guides/san_francisco/js/scripts.js
+++ weekend_guides/san_francisco/js/scripts.js
@@ -1,148 +1,98 @@
-/*function preloadImages(srcs) {
-	if (!preloadImages.cache) {
-		preloadImages.cache = [];
-	}
-	var img;
-	for (var i = 0; i < srcs.length; i++) {
-        img = new Image();
-        img.src = srcs[i];
-        console.log(i)
-        preloadImages.cache.push(img);
-    }
-}
+var map;
+var things_to_see = [
+	{label: 'A', name: 'Lands End', position: {lat: 37.779702, lng: -122.511589}},
+	{label: 'B', name: 'Union Square', position: {lat: 37.787994, lng: -122.407437}},
+	{label: 'C', name: 'The Mission', position: {lat: 37.759716 , lng: -122.419458}},
+	{label: 'D', name: 'Twin Peaks', position: {lat:  37.754407, lng: -122.447684}},
+]
 
-function isMobile() {
-	return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-}
+var things_to_eat = [
+	{label: 'E', name: 'Brenda\'s French Soul Food', position: {lat: 37.7829023, lng: -122.41903580000002}},
+	{label: 'F', name: 'Ramen Yamadaya', position: {lat: 37.7860697, lng: -122.42970700000001}},
+	{label: 'G', name: 'Nopalito', position: {lat: 37.7733608 , lng: -122.43898509999997}},
+	{label: 'H', name: 'Yank Sing', position: {lat:  37.7924983, lng: -122.39313870000001}},
+	{label: 'I', name: 'Taquería El Farolito', position: {lat: 37.752654, lng: -122.41819150000003}}
+]
 
-var food_loaded = true
-var sights_loaded = true
-var nightlife_loaded = true
+var markers = []
 
-$(document).ready(function(){
-
-	var foodSrcs = ["images/brendas.jpg", "images/yamadaya.jpg", "images/nopalito.jpg", "images/el_farolito.jpg", "images/yank_sing.jpg"];
-
-	$('#brendas').hover(function(){
-		$('#food_image').css("background-image","url(images/brendas.jpg)")
+function loadItems(items, map){
+	markers.forEach(function(marker) {
+		//marker.setMap(null)
 	})
-
-	$('#yamadaya').hover(function(){
-		$('#food_image').css("background-image","url(images/yamadaya.jpg)")
-	})
-
-	$('#nopalito').hover(function(){
-		$('#food_image').css("background-image","url(images/nopalito.jpg)")
-	})
-
-	$('#el_farolito').hover(function(){
-		$('#food_image').css("background-image","url(images/el_farolito.jpg)")
-	})
-
-	$('#yank_sing').hover(function(){
-		$('#food_image').css("background-image","url(images/yank_sing.jpg)")
-	})
-
-	$('#food_content').hover(function(){
-		if(!isMobile() && food_loaded) {
-			food_loaded = !food_loaded
-			preloadImages(foodSrcs)
-		}
-	}, function(){
-		$('#food_image').css("background-image","url(images/food.jpg)")
-	})
-
-	var sightsSrcs = ["images/golden_gate_bridge.jpg", "images/alcatraz.jpg", "images/twin_peaks.jpg", "images/exploratorium.jpg", "images/mission.jpg", , "images/painted_ladies.jpg"];
-
-	$('#golden_gate_bridge').hover(function(){
-		$('#sights_image').css("background-image","url(images/golden_gate_bridge.jpg)")
-	})
-
-	$('#alcatraz').hover(function(){
-		$('#sights_image').css("background-image","url(images/alcatraz.jpg)")
-	})
-
-	$('#twin_peaks').hover(function(){
-		$('#sights_image').css("background-image","url(images/twin_peaks.jpg)")
-	})
-
-	$('#exploratorium').hover(function(){
-		$('#sights_image').css("background-image","url(images/exploratorium.jpg)")
-	})
-
-	$('#mission').hover(function(){
-		$('#sights_image').css("background-image","url(images/mission.jpg)")
-	})
-
-	$('#painted_ladies').hover(function(){
-		$('#sights_image').css("background-image","url(images/painted_ladies.jpg)")
-	})
-
-	$('#sights_content').hover(function(){
-		if(!isMobile() && sights_loaded) {
-			preloadImages(sightsSrcs)
-			sights_loaded = !sights_loaded
-		}
-	}, function(){
-		$('#sights_image').css("background-image","url(images/sights.jpg)")
-	})
-
-	var nightlifeSrcs = ["images/holy_water.jpg", "images/li_po.jpg", "images/dna_lounge.jpg", "images/spin.jpg"];
-
-	$('#holy_water').hover(function(){
-		$('#nightlife_image').css("background-image","url(images/holy_water.jpg)")
-	})
-
-	$('#li_po').hover(function(){
-		$('#nightlife_image').css("background-image","url(images/li_po.jpg)")
-	})
-
-	$('#dna_lounge').hover(function(){
-		$('#nightlife_image').css("background-image","url(images/dna_lounge.jpg)")
-	})
-
-	$('#spin').hover(function(){
-		$('#nightlife_image').css("background-image","url(images/spin.jpg)")
-	})
-
-	$('#nightlife_content').hover(function(){
-		if(!isMobile() && nightlife_loaded) {
-			preloadImages(nightlifeSrcs)
-			nightlife_loaded = !nightlife_loaded
-		}
-	}, function(){
-		$('#nightlife_image').css("background-image","url(images/nightlife.jpg)")
-	})
-
-	jQuery('img.svg').each(function(){
-		var $img = jQuery(this);
-		var imgID = $img.attr('id');
-		var imgClass = $img.attr('class');
-		var imgURL = $img.attr('src');
-		console.log("hii")
-		jQuery.get(imgURL, function(data) {
-			console.log("byee")
-			// Get the SVG tag, ignore the rest
-			var $svg = jQuery(data).find('svg');
-
-			// Add replaced image's ID to the new SVG
-			if(typeof imgID !== 'undefined') {
-					$svg = $svg.attr('id', imgID);
+	markers = []
+	items.forEach(function(thing) {
+		var marker = new google.maps.Marker({
+			position: thing.position,
+			map: map,
+			label: {
+				text: thing.label,
+				color: '#ff7665',
+				fontSize: '13px',
+				fontFamily: 'Proxima_Nova',
+				fontWeight: '600'
+			},
+			icon: {
+				url: 'https://i.imgur.com/q9kann7.png',
+				labelOrigin: new google.maps.Point(12, 13)
 			}
-			// Add replaced image's classes to the new SVG
-			if(typeof imgClass !== 'undefined') {
-				$svg = $svg.attr('class', imgClass+' replaced-svg');
-			}
-
-			// Remove any invalid XML tags as per http://validator.w3.org
-			$svg = $svg.removeAttr('xmlns:a');
-
-			// Replace image with new SVG
-		$img.replaceWith($svg);
-
-		}, 'xml');
-
+		});
+		var infowindow = new google.maps.InfoWindow({
+			content: "<div><h3 style='margin:0;'>" + thing.name + "</h3></div>"
+		})
+		marker.addListener('mouseover', function(){
+			infowindow.open(map, marker)
+		})
+		marker.addListener('mouseout', function(){
+			infowindow.close()
+		})
+		markers.push(marker)
 	});
+}
 
+function createMap(div, loc, zoom){
+	return new google.maps.Map(document.getElementById(div), {
+		center: loc,
+		zoom: zoom ,
+		styles: [{"elementType":"labels","stylers":[{"color":"#f3f3f3"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#949494"},{"weight":2.5}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"administrative.land_parcel","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#ff766d"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"weight":0.5}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.local","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"color":"#54dede"}]}],
+		mapTypeControl: false,
+		scaleControl: false,
+		streetViewControl: false,
+		rotateControl: false,
+		fullscreenControl: false,
+		zoomControl: false,
+	});
+}
+
+function initMap() {
+	map = createMap('map', {lat: 37.773972, lng: -122.431297}, 12)
+
+	loadItems(things_to_see, map)
+	loadItems(things_to_eat, map)
+}
+
+function loadItin(data){
+	Object.entries(data.itineraries).forEach(function(thing){
+		content_string = '<a href="../itinerary#san_francisco/' + thing[0] +'"><div class="itinerary"><img src="./'+ thing[1].itinerary[0].image + '"><h2>' + thing[1].name + '</h2></div></a>'
+		$('#itineraries').append(content_string)
+	})
+}
+
+$.ajax({
+	dataType: "json",
+	url: "./itinerary.json",
+	success: function(data){
+		loadItin(data)
+	}, error: function(data){
+		loadItin({"location": "San Francisco","itineraries": {"downtown": {"name": "Downtown San Francisco","itinerary": [{"name": "Union Square","location": {"lat": 37.7879938,"lng": -122.40743739999999},"label": "A","type": "sight","cost": "Free","time": "30 minutes","description": "A great place to start a trip in SF, surrounded by department stores and hotels.","link": "","link_text": "","image": "images/union_square.jpg"},{"type": "walk","duration": "5 minute walk"},{"name": "Asha Tea House","location": {"lat": 37.78818,"lng": -122.40369599999997},"label": "B","type": "food","cost": "$","description": "A wonderful tea house with high quality drinks.","link": "https://www.ashateahouse.com/","link_text": "Website","image": "images/asha.jpg"},{"type": "walk","duration": "5 minute walk"},{"name": "SF Museum of Modern Art","location": {"lat": 37.7857182,"lng": -122.40105080000001},"label": "C","type": "sight","cost": "$25","time": "2 - 3 hours","description": "A modern art museum with a beautiful collection.","link": "Website","link_text": "https://www.sfmoma.org/","image": "images/sfmoma.jpg"},{"type": "walk","duration": "2 minute walk"},{"name": "Samovar Tea Lounge","location": {"lat": 37.78417,"lng": -122.40210139999999},"label": "D","type": "food","cost": "$$","description": "A large selection of tasty food paired with tea.","link": "https://www.samovartea.com/yerba-buena-gardens/","link_text": "Website","image": "images/samovar.jpg"},{"type": "train","duration": "17 minute train ride"},{"name": "San Francisco Ferry Building","location": {"lat": 37.7955169,"lng": -122.39347279999998},"label": "E","type": "sight","cost": "free","time": "30 minutes","description": "A historic ferry terminal with a variety of artisanal shops are restaurants.","link": "Website","link_text": "https://www.ferrybuildingmarketplace.com/","image": "images/ferry_building.jpg"},{"type": "walk","duration": "9 minute walk"},{"name": "Exploratorium","location": {"lat": 37.801542,"lng": -122.39748400000002},"label": "F","type": "sight","cost": "free","time": "2 hours","description": "A museum with hundreds of wonderful interactive exhibits for both adults and kids/","link": "Website","link_text": "www.exploratorium.edu","image": "images/exploratorium.jpg"},]},"mission": {"name": "The Mission District","itinerary": [{"name": "Mission Dolores Park","location": {"lat": 37.7596168,"lng": -122.42690379999999},"label": "A","type": "sight","cost": "Free","time": "1.5 hours","description": "Bustling park where people gather for picincs and relaxation.","link": "http://sfrecpark.org/destination/mission-dolores-park/","link_text": "sfrecpark.org","image": "images/mission_park.jpg"},{"type": "walk","duration": "19 minute walk"},{"name": "Taquería El Farolito","location": {"lat": 37.752654782767,"lng": -122.41819202899933},"label": "B","type": "food","cost": "$$","description": "Busy Mexican restaurant serving comfort food.","link": "http://places.singleplatform.com/el-farolito-3/menu?ref=google","link_text": "Menu","image": "images/el_farolito.jpg"},{"type": "walk","duration": "8 minute walk"},{"name": "Mission District Murals","location": {"lat": 37.752420993840076,"lng": -122.41570642570304},"label": "C","type": "sight","cost": "Free","time": "1.5 hours","description": "A multitude of everchanging murals located in the Mission.","link": "","link_text": "","image": "images/mission_murals.jpg"},{"type": "walk","duration": "14 minute walk"},{"name": "Foreign Cinema Restaurant","location": {"lat": 37.7564724,"lng": -122.41928239999999},"label": "D","type": "food","cost": "$$$","description": "Californian-Mediterranean food in an outdoor space screening films.","link": "Website","link_text": "foreigncinema.com","image": "images/foreign_cinema.jpg"},{"type": "walk","duration": "14 minute walk"},{"name": "Bi Rite Creamery","location": {"lat": 37.76158999999999,"lng": -122.42571700000002},"label": "E","type": "food","cost": "$","description": "Homemade organic ice cream with often long lines.","link": "Website","link_text": "biritecreamery.com","image": "images/bi_rite.jpg"},{"type": "drive","duration": "12 minute drive"},{"name": "Twin Peaks","location": {"lat": 37.7544066,"lng": -122.44768449999998},"label": "C","type": "sight","cost": "Free","time": "30 minutes","description": "An amazing, 180 degree view of the Bay Area.","link": "","link_text": "","image": "images/twin_peaks.jpg"}]}},"map": {"location": {"lat": 37.75777584134365,"lng": -122.4341467629751},"zoom": 13,"styles": [{"elementType": "labels","stylers": [ {"color": "#f3f3f3" }] }, {"elementType": "labels.text.stroke","stylers": [{"color": "#949494" }, {"weight": 2.5 }] }, {"featureType": "administrative","elementType": "geometry","stylers": [ {"visibility": "off" }]},{"featureType": "administrative.land_parcel","elementType": "labels","stylers": [ {"visibility": "off" }] }, {"featureType": "landscape","elementType": "geometry","stylers": [ {"color": "#ff766d" }] }, {"featureType": "poi","stylers": [ {"visibility": "off" }] }, {"featureType": "poi","elementType": "labels.text","stylers": [ {"visibility": "off" }] }, {"featureType": "road","elementType": "geometry.fill","stylers": [ {"color": "#ffffff" }] }, {"featureType": "road","elementType": "geometry.stroke","stylers": [ {"color": "#ffffff" }, {"weight": 0.5 }] }, {"featureType": "road","elementType": "labels.icon","stylers": [ {"visibility": "off" }] }, {"featureType": "road.arterial","elementType": "labels","stylers": [ {"visibility": "off" }] }, {"featureType": "road.highway","elementType": "labels","stylers": [ {"visibility": "off" }] }, {"featureType": "road.local","stylers": [ {"visibility": "off" }] }, {"featureType": "road.local","elementType": "labels","stylers": [ {"visibility": "off" }] }, {"featureType": "transit","stylers": [ {"visibility": "off" }] }, {"featureType": "water","stylers": [ {"color": "#54dede" }]  }]}})
+	}
 })
-*/
+
+$(window).on('scroll', function() {
+	var window_top = $(window).scrollTop();
+	var div_top = $('#map_anchor').offset().top;
+	var stop_top = $('#test').offset().top - $('#map_section').height();
+	if (window_top > stop_top) {
+		$('#map_section').css({top: stop_top - window_top + 'px'});
+	}
+});
